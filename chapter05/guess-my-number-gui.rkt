0c3b5a62f0603323f38cac06cdca3833157f2b99
Benjamin <benjamin.guy.thomas@gmail.com>
2023-01-29 15:17:40 +0100

    Move

--- chapter05/guess-my-number-gui.rkt
+++ chapter05/guess-my-number-gui.rkt
@@ -2,20 +2,17 @@
 
 ; 2htdp stands for: How to Design Programs, Second Edition
 ; See: https://htdp.org
-(require 2htdp/universe 2htdp/image)
+(require 2htdp/universe
+         2htdp/image)
 
 (define WIDTH 480)
 (define HEIGHT 600)
-(define UFO (bitmap "ufo.96x96.png"))
+(define UFO (bitmap "img/ufo.96x96.png"))
 
 (define (update model)
   (+ model 3))
 
 (define (view model)
-  (place-image UFO (/ WIDTH 2) model
-               (empty-scene WIDTH HEIGHT)))
+  (place-image UFO (/ WIDTH 2) model (empty-scene WIDTH HEIGHT)))
 
-(big-bang 0
-  (on-tick update)
-  (to-draw view))
-
+(big-bang 0 (on-tick update) (to-draw view))
