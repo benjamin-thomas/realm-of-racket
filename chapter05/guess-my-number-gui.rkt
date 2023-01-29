340ae1b786b8133aaedf9eedfb149d7bd3a56190
Benjamin <benjamin.guy.thomas@gmail.com>
2023-01-29 15:16:14 +0100

    Add game basic scaffolding

---
+++ chapter05/guess-my-number-gui.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+; 2htdp stands for: How to Design Programs, Second Edition
+; See: https://htdp.org
+(require 2htdp/universe 2htdp/image)
+
+(define WIDTH 480)
+(define HEIGHT 600)
+(define UFO (bitmap "ufo.96x96.png"))
+
+(define (update model)
+  (+ model 3))
+
+(define (view model)
+  (place-image UFO (/ WIDTH 2) model
+               (empty-scene WIDTH HEIGHT)))
+
+(big-bang 0
+  (on-tick update)
+  (to-draw view))
+
